b90859dfe8988ecb42a44f3409c99ffeff88749f
Ashivu <144000515+Ashivu@users.noreply.github.com>
2023-11-10 15:56:09 +1100

    Added unittests

--- test/test_operators.py
+++ test/test_operators.py
@@ -18,6 +18,28 @@ class TestOperators(unittest.TestCase):
         self.assertEqual(arrays[1], None)
         self.assertTrue(np.allclose(nu, np.array([[0.5, 0.0], [0.0, -0.5]])))
 
+    def test_simple_solid_fraction(self):
+        s = np.array([[[2,np.nan],[3,1]], [[1,1],[4,np.nan]]])
+
+        s_bar = operators.get_solid_fraction(s,1,1)
+        
+        self.assertEqual(s_bar,0.5)
+
+    def test_simple_get_average(self):
+        s = np.array([[[2,np.nan,np.nan],[3,3,3]], [[1,1,np.nan],[4,np.nan,np.nan]]])
+
+        s_mean = operators.get_average(s)
+
+        self.assertEqual(s_mean[0],1.5)
+        self.assertEqual(s_mean[1],3.5)
+
+    def test_get_hyperbolic_average(self):
+        s = np.array([[[2,np.nan],[3,1]], [[1,1],[4,np.nan]]])
+
+        s_inv_bar = operators.get_hyperbolic_average(s)
+        self.assertEqual(s_inv_bar[0][0],2)
+        self.assertEqual(s_inv_bar[1][1],4)
+
 
 if __name__ == "__main__":
     unittest.main()
